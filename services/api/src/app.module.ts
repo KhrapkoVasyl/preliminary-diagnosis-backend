d15f0c409d241d567a102bd4c159f4702c8e06f1
KhrapkoVasyl <khrapko2002@gmail.com>
2024-05-14 14:00:16 +0300

    chore(api): setup typeorm db connection

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './systems/database';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- services/api/src/config/app-config.service.ts
+++ services/api/src/config/app-config.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'node:path';
 import { NodeEnvEnum } from 'src/common/enums';
 
 @Injectable()
@@ -19,5 +21,27 @@ export class AppConfigService {
 
   public isNodeEnv(mode: NodeEnvEnum): boolean {
     return this.get('NODE_ENV') === mode;
+  }
+
+  getDbConfig(): TypeOrmModuleOptions {
+    return {
+      type: this.get<'postgres'>('TYPEORM_TYPE'),
+      name: this.get('TYPEORM_CONNECTION_NAME'),
+      host: this.get('TYPEORM_HOST'),
+      port: this.get('TYPEORM_PORT'),
+      cache: this.get('TYPEORM_CACHE'),
+      logging: this.get('TYPEORM_LOGGING'),
+      database: this.get<string>('TYPEORM_DATABASE'),
+      username: this.get('TYPEORM_USERNAME'),
+      password: this.get<string>('TYPEORM_PASSWORD'),
+      extra: {
+        ssl: this.get('TYPEORM_SSL'),
+      },
+      dropSchema: this.get('TYPEORM_DROP_SCHEMA'),
+      synchronize: this.get('TYPEORM_SYNCHRONIZE'),
+      migrationsRun: this.get('TYPEORM_MIGRATIONS_RUN'),
+      entities: [join(__dirname, '../modules/**/*.entity.{ts,js}')],
+      migrations: [join(__dirname, '../systems/database/migrations/*.{ts,js}')],
+    };
   }
 }
