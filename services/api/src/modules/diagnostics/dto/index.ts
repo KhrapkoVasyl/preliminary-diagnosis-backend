06e4693f7642a843af4f411b230cbad1ba03c2ce
KhrapkoVasyl <khrapko2002@gmail.com>
2024-06-08 20:54:48 +0300

    feat(diagnostics): remove updation of diagnostic

--- services/api/src/modules/diagnostics/dto/index.ts
+++ services/api/src/modules/diagnostics/dto/index.ts
@@ -1,2 +1,1 @@
 export * from './create-diagnostic.dto';
-export * from './update-diagnostic.dto';
--- services/api/src/modules/diagnostics/dto/update-diagnostic.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDiagnosticDto } from './create-diagnostic.dto';
-
-export class UpdateDiagnosticDto extends PartialType(CreateDiagnosticDto) {}
