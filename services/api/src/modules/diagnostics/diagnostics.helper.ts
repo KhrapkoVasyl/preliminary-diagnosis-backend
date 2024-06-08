1609257698cb7e48de221053221b8115314a9769
KhrapkoVasyl <khrapko2002@gmail.com>
2024-06-08 14:39:24 +0300

    refactor(diagnostics): replaced hardcoded value with actual result status value

--- services/api/src/modules/diagnostics/diagnostics.helper.ts
+++ services/api/src/modules/diagnostics/diagnostics.helper.ts
@@ -1,9 +1,10 @@
+import { DiagnosticResultStatus } from '../diagnostic-results/enums';
 import { DiagnosticStatus } from './enums';
 
 export const getDiagnosticStatusQuery = (alias: string) => `
 CASE
-  WHEN (SELECT COUNT(*) FROM diagnostic_results dr WHERE dr.diagnosticId = ${alias}.id AND dr.status = 'FAILED') > 0 THEN '${DiagnosticStatus.COMPLETED_WITH_ISSUES}'
-  WHEN (SELECT COUNT(*) FROM diagnostic_results dr WHERE dr.diagnosticId = ${alias}.id AND dr.status != 'COMPLETED') = 0 THEN '${DiagnosticStatus.COMPLETED}'
+  WHEN (SELECT COUNT(*) FROM diagnostic-results dr WHERE dr.diagnosticId = ${alias}.id AND dr.status = '${DiagnosticResultStatus.FAILED}') > 0 THEN '${DiagnosticStatus.COMPLETED_WITH_ISSUES}'
+  WHEN (SELECT COUNT(*) FROM diagnostic-results dr WHERE dr.diagnosticId = ${alias}.id AND dr.status != '${DiagnosticResultStatus.COMPLETED}') = 0 THEN '${DiagnosticStatus.COMPLETED}'
   ELSE '${DiagnosticStatus.PENDING}'
 END
 `;
