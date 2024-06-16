67b61561f5b9f4dd94d751c06d9385a74e230ad3
KhrapkoVasyl <khrapko2002@gmail.com>
2024-06-16 12:35:55 +0300

    feat: restrict access to files

--- services/api/src/modules/files/files.controller.ts
+++ services/api/src/modules/files/files.controller.ts
@@ -10,17 +10,20 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileEntity } from './file.entity';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CreateFileDto } from './dto';
 import { IdDto } from 'src/common/dto';
+import { Role } from '../auth/decorators';
+import { UserRoleEnum } from '../users/enums';
 
 @ApiTags('files')
 @Controller('files')
-// @ApiBearerAuth()
+@ApiBearerAuth()
 @UseInterceptors(ClassSerializerInterceptor)
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Role(UserRoleEnum.ADMIN)
   @Post()
   @ApiConsumes('multipart/form-data')
   @ApiBody({ type: CreateFileDto })
@@ -28,16 +31,19 @@ export class FilesController {
     return this.filesService.createOne(dto.file);
   }
 
+  @Role(UserRoleEnum.ADMIN)
   @Get()
   async findAll(): Promise<FileEntity[]> {
     return this.filesService.findAll();
   }
 
+  @Role(UserRoleEnum.ADMIN)
   @Get(':id')
   async findOne(@Param() conditions: IdDto): Promise<FileEntity> {
     return this.filesService.findOne(conditions);
   }
 
+  @Role(UserRoleEnum.ADMIN)
   @Delete(':id')
   async deleteOne(@Param() conditions: IdDto) {
     return this.filesService.deleteOne(conditions);
