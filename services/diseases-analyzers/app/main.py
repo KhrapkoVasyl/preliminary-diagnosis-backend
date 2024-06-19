2083b96bb6731c24feb7630834b3d335babcd560
KhrapkoVasyl <khrapko2002@gmail.com>
2024-06-20 01:33:28 +0300

    chore: add logging of env starting params

--- services/diseases-analyzers/app/main.py
+++ services/diseases-analyzers/app/main.py
@@ -1,16 +1,24 @@
 import logging
 import time
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from app.services.rabbitmq_connection_service import RabbitMQConnectionService
 from app.services.rabbitmq_consumer_service import RabbitMQConsumerService
 from app.services.rabbitmq_publisher_service import RabbitMQPublisherService
 from app.services.storage_service import StorageService
 from app.services.disease_analyzer_service import DiseaseAnalyzerService
+from app.config import ENV_FILE
+
+load_dotenv(ENV_FILE)
 
 time.sleep(17)
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+logger.debug(f"ENV PARAM - MODEL_PATH: {os.getenv('MODEL_PATH')}")
+logger.debug(f"ENV PARAM - RABBIT_MQ_QUEUE_NAME: {os.getenv('RABBIT_MQ_QUEUE_NAME')}")
 
 app = FastAPI()
 
---
+++ services/diseases-analyzers/app/services/disease_analyze_service.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import io
+import os
+import logging
+from app.services.storage_service import StorageService
+from dotenv import load_dotenv
+import tempfile
+from app.config import ENV_FILE
+
+load_dotenv(ENV_FILE)
+
+class DiseaseAnalyzerService:
+    def __init__(self, storage_service: StorageService):
+        self.logger = logging.getLogger(__name__)
+        self.storage_service = storage_service
+        self.model = self.load_model(os.getenv('MODEL_PATH'))
+
+    def load_model(self, model_path):
+        try:
+            self.logger.debug(f"Loading model from: {model_path}")
+            buffer = self.storage_service.read_file_to_buffer(model_path)
+            with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
+                temp_model_file.write(buffer.getvalue())
+                temp_model_path = temp_model_file.name
+            model = tf.keras.models.load_model(temp_model_path)
+            self.logger.info("Model loaded successfully")
+            os.remove(temp_model_path)
+            return model
+        except Exception as e:
+            self.logger.error(f"Failed to load model: {e}")
+            raise e
+
+    def analyze_disease(self, image_buffer):
+        try:
+            image = tf.io.decode_image(image_buffer.getvalue(), channels=3)
+            image = tf.image.resize(image, [224, 224])
+            image = tf.expand_dims(image, axis=0)
+            prediction = self.model.predict(image)
+            disease_probability = prediction[0][0]
+            self.logger.debug(f"Disease probability: {disease_probability}")
+            return disease_probability
+        except Exception as e:
+            self.logger.error(f"Failed to analyze disease: {e}")
+            raise e
